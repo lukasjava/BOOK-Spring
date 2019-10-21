104218936c920fb94492f456ec93ef837b206812
lukas_000 <zaborowski.luk@gmail.com>
2019-10-21 15:57:40 +0200

    first commit

---
+++ LukaszApplication.java
@@ -0,0 +1,13 @@
+package com.example.Lukasz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LukaszApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LukaszApplication.class, args);
+	}
+
+}
